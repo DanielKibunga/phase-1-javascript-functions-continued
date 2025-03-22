9da2f0e60ca7df2e86ca03e70c4d5d41c21604f2
Daniel Kimani <daniel.kibunga@students.moringaschool.com>
2025-03-22 08:14:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun("watch movies"));
+
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+const highlight = wrapAdjective("||");
+console.log(highlight("a genius"));
